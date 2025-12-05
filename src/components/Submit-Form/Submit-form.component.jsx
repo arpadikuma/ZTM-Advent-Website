da5024d6cd1ff019e44444abe92ea4f48540e911
arpadikuma <arpadikuma@gmail.com>
2025-12-05 21:54:18 +0100

    Adjusted date range to reflect 2025 changes

--- src/components/Submit-Form/Submit-form.component.jsx
+++ src/components/Submit-Form/Submit-form.component.jsx
@@ -231,7 +231,7 @@ function SubmitForm({ user }) {
 	const populateDates = () => {
 		let i;
 		const list = [];
-		for (i = 1; i < 26; i++) {
+		for (i = 1; i < 13; i++) {
 			list.push(i);
 		}
 		return list;
